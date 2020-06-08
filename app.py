ebdc5b29071fb4ad28175f96cbcde911d9853c8e
Harsh Patel <harsh@harsh.local>
2020-06-08 13:50:57 +0530

    Adding Delete functionality V-3

--- app.py
+++ app.py
@@ -1,65 +1,102 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'thisisasecret'
+
 db = SQLAlchemy(app=app)
+
 
 class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    city_set = set()
-    weather_data = []
-    new_city_flag = False
-    city_error = False
-
-    cities = City.query.all()
-    for city in cities:
-        city_set.add(city.name)
-
-    if request.method == 'POST':
-        new_city = request.form.get('city')
-        new_city.capitalize()
-
-        if new_city not in city_set:
-            city_set.add(new_city)
-            new_city_flag = True
-
+def get_weather_data(city):
     website = 'https://api.openweathermap.org/data'
     version = '2.5'
     unit = 'metric'
     api = '8b29b1101ffeda5bb8c2d72994e14df5'
 
-    for city in city_set:
-        url = f"{website}/{version}/weather?q={city}&units={unit}&appid={api}"
-        r = requests.get(url=url).json()
+    url = f"{website}/{version}/weather?q={city}&units={unit}&appid={api}"
+    r = requests.get(url=url).json()
 
-        if r['cod'] == 200:
-            weather = {
-                'city': r["name"],
-                'temperature': r["main"]["temp"],
-                'description': r["weather"][0]["description"],
-                'icon': r["weather"][0]["icon"],
-            }
+    if r['cod'] == 200:
+        weather = {
+            'city': r["name"],
+            'temperature': r["main"]["temp"],
+            'description': r["weather"][0]["description"],
+            'icon': r["weather"][0]["icon"],
+        }
+
+    else:
+        weather = None
+
+    return weather
+
+
+@app.route('/')
+def index():
+    weather_data = []
+
+    cities = City.query.all()
+    for city in cities:
+        weather = get_weather_data(city=city.name)
+
+        if weather:
             weather_data.append(weather)
-            if new_city_flag is True:
-                new_city_object = City(name=city)
-                db.session.add(new_city_object)
-                db.session.commit()
-
-        else:
-            city_error = True
-
-    if city_error:
-        city_set.remove(new_city)
 
     return render_template('index.html', weather_data=weather_data)
+
+
+@app.route('/', methods=['POST'])
+def form_data():
+    err_msg = ''
+    new_city = request.form.get('city')
+    new_city.capitalize()
+
+    if new_city:
+        existing_city = City.query.filter_by(name=new_city).first()
+
+        if not existing_city:
+            new_city_status = get_weather_data(new_city)
+
+            if new_city_status:
+                new_city_obj = City(name=new_city.lower())
+                db.session.add(new_city_obj)
+                db.session.commit()
+            else:
+                err_msg = 'City does not exist in the world!!'
+        else:
+            err_msg = 'City already exist in the database!!'
+    else:
+        err_msg = 'Please enter a city name!!'
+
+    if err_msg:
+        flash(err_msg, 'error')
+    else:
+        flash('City added succesfully!!', 'success')
+
+    return redirect(url_for('index'))
+
+
+@app.route('/delete/<name>')
+def delete_city(name):
+    try:
+        city = City.query.filter_by(name=name.lower()).first()
+        db.session.delete(city)
+        db.session.commit()
+
+        success_msg = f'Successfully deleted {name}!!'
+
+        flash(success_msg, 'success')
+
+    except Exception as e:
+        flash(f'{city} not found in database!', 'error')
+    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
